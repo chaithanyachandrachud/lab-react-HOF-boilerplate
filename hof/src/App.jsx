5889c80c5fbf72fa4388280389830d0b865503c3
chaithanyachandrachud <chaithanya.c@kalvium.community>
2024-01-08 14:36:29 +0530

    done

---
+++ hof/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import HigherOrderFunction from "./components/HighOrderFunction";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <HigherOrderFunction />
+      </div>
+    </>
+  );
+}
+
+export default App;
